bf6a0cf2dcd6fbf3f5efc5ab7852217e6892c095
senam <senamdk16@gmail.com>
2025-09-22 13:20:02 +0000

    end portfolio of portfolio

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-const btnMenu = document.getElementById("btnMenu")
-const navMobile = document.getElementById("navMobile")
-const itemMenuMobile = document.querySelectorAll("#navMobile a")
+const menuButton = document.getElementById("menu-button");
+const mobileMenu = document.getElementById("mobile-menu");
+const form = document.querySelector("#form");
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const inputMessage = document.getElementById("message");
+const numero = "+22870684925";
 
+const sendToWhatsApp = (_name, _email, _message, _numero) => {
+  const fullMessage = `Nom: ${_name} \n Email: ${_email} \n Message: ${_message}`;
+  const encodageMessage = encodeURIComponent(fullMessage);
+  const urlWhatsApp = `https://wa.me/${numero}?text=${encodageMessage}`;
 
-btnMenu.addEventListener("click",()=>{
-    if(btnMenu.ariaExpanded == "false" ){
-        btnMenu.ariaExpanded = "true"
-        navMobile.classList.remove("translate-x-full")
-    }
-    else{
-        btnMenu.ariaExpanded = "false"
-        navMobile.classList.add("translate-x-full")
-    }
-})
+  window.open(urlWhatsApp, "_blank");
+};
+menuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
 
-itemMenuMobile.forEach((item)=>{
-    item.addEventListener("click",()=>{
-        if(btnMenu.ariaExpanded == "true"){
-            btnMenu.ariaExpanded = "false"
-            navMobile.classList.add("translate-x-full")
-        }
-    })
-})
+// Optionnel : fermer le menu quand on clique sur un lien
+const menuLinks = document.querySelectorAll("#mobile-menu a");
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  console.log("submit");
+  let name = inputName.value;
+  let email = inputEmail.value;
+  let message = inputMessage.value;
+  sendToWhatsApp(name, email, message, numero);
+  inputName.value = "";
+  inputEmail.value = "";
+  inputMessage.value = "";
+});
