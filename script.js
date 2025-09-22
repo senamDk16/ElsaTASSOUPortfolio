3bc8d92e5e2fb1a4989068452c86dc3c5ae788cb
senam <senamdk16@gmail.com>
2025-09-22 09:40:13 +0000

    new direction of portfolio

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-const menuToggle = document.querySelector("#menu-toggle")
-const menuMobile = document.querySelector("#mobile-menu")
-const showMenu = ()=>{
-    menuMobile.classList.toggle("")
-}
+const btnMenu = document.getElementById("btnMenu")
+const navMobile = document.getElementById("navMobile")
+const itemMenuMobile = document.querySelectorAll("#navMobile a")
+
+
+btnMenu.addEventListener("click",()=>{
+    if(btnMenu.ariaExpanded == "false" ){
+        btnMenu.ariaExpanded = "true"
+        navMobile.classList.remove("translate-x-full")
+    }
+    else{
+        btnMenu.ariaExpanded = "false"
+        navMobile.classList.add("translate-x-full")
+    }
+})
+
+itemMenuMobile.forEach((item)=>{
+    item.addEventListener("click",()=>{
+        if(btnMenu.ariaExpanded == "true"){
+            btnMenu.ariaExpanded = "false"
+            navMobile.classList.add("translate-x-full")
+        }
+    })
+})
